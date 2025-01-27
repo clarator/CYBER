cf36f57b9bdf23d6bc95e35cf3e22a6113435cc2
clarator <116170772+clarator@users.noreply.github.com>
2025-01-27 14:34:00 +0100

    test

--- ex_2/1/test.php
+++ ex_2/1/test.php
@@ -9,7 +9,7 @@ libxml_disable_entity_loader(true);
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT);
+$document->loadXML($xml);
 $parsedDocument = simplexml_import_dom($document);
 
 echo $parsedDocument->content;
